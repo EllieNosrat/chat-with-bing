34fe16472e9c4d3cbf37e6240c3eb87c8d8a7f67
Pooya Alavian <pooyaalavian@microsoft.com>
2025-03-04 10:23:42 -0500

    add azure fn

---
+++ function_app.py
@@ -0,0 +1,57 @@
+import azure.functions as func
+import json 
+import logging
+from src import chat
+import uuid
+from datetime import datetime, timedelta
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+
+memory = {
+    '1':{'messages':[],'last_modified':0}
+}
+
+@app.route(route="chat")
+def chat(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    body = req.get_body().decode('utf-8')
+    body = json.loads(body)
+    user_message = body['user_message']
+    conversation_id = body.get('conversation_id', None)
+    
+    if conversation_id is None:
+        conversation_id = uuid.uuid4().hex
+    if conversation_id not in memory:
+        memory[conversation_id] = {
+            'messages':[ {'role':'system','content':"You are a financial advice agent. You have access to sec.gov and finra.org for regulatory information. If you don't know the answer to a user's quesiton, use the search tool that will perform seach n these websites. Always cite your answers using the id provided in sources, for example <cite id=\"1\"/>. The answers must be tailored to US customers. Do not include site: in your search query."},],
+            'last_modified':datetime.now(),
+        }
+    memory[conversation_id]['messages'].append({'role':'user','content':user_message})  
+    memory[conversation_id]['last_modified'] = datetime.now()
+    res = chat(messages=memory[conversation_id]['messages'])
+    memory[conversation_id]['messages'].append(res)
+    
+    return func.HttpResponse(
+        json.dumps({
+            'answer':res['content'],
+            'conversation_id':conversation_id
+        }),
+        mimetype="application/json",
+        status_code=200
+    )
+    
+
+
+@app.timer_trigger(schedule="0 10 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def clean_up_memory(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    now = datetime.now()
+    for k in list(memory.keys()):
+        if now - memory[k]['last_modified'] > timedelta(minutes=30):
+            del memory[k]
+    
---
+++ src/__init__.py
@@ -0,0 +1,106 @@
+from openai import AzureOpenAI
+import os 
+import requests
+from bs4 import BeautifulSoup
+import re 
+
+bing_grounded_sites = [
+    'sec.gov',
+    'finra.org/rules-guidance/rulebooks',
+]
+
+
+client = AzureOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+)
+
+def bing_search(query):
+    sites_str= ' OR '.join([f'site:{s}' for s in bing_grounded_sites])
+    url = f'v7.0/search?q={query} ({sites_str})'
+    print(f'Searching Bing: {url}')
+    response = requests.get(f'{os.getenv("BING_ENDPOINT")}{url}', headers={'Ocp-Apim-Subscription-Key': os.getenv("BING_API_KEY")})
+    return response
+
+def get_result_clean_text(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for el in soup(['script', 'style', 'header', 'footer']):
+        el.decompose()
+
+    for el in soup.find_all(class_='sidebar'):
+        el.decompose()
+
+    text_content = soup.get_text()
+    text_content = re.sub(r' {2,}', ' ', text_content)
+    text_content = re.sub(r'\n{3,}', '\n\n', text_content)
+
+    print(f'Extracted {len(text_content)} characters from {url}')
+
+    return text_content
+
+def search_and_get_text(query):
+    context = []
+    response = bing_search(query)
+    data = response.json()
+    id = 1
+    if 'webPages' in data:
+        for page in data['webPages']['value']:
+            url = page['url']
+            text = get_result_clean_text(url)
+            context.append({'id':id, 'src':url, 'content':text})
+            id+=1
+    
+    # serialize answer
+    srcs = '\n'.join([f'<source id=="{c["src"]}">\n{c["content"]}\n</source>' for c in context])
+    srcs_short = '\n'.join([f'<source id="{c["src"]}">\n{len(c["content"])}\n</source>' for c in context])
+    print(f'<sources>{srcs_short}</sources>')
+    return f'<sources>{srcs}</sources>'
+
+
+bing_tool_def = {
+    'type':'function',
+    'function':{
+        'name':'search_and_get_text',
+        # 'description':f'Searches {", ".join(bing_grounded_sites)} for a query and returns the text content of the first few results',
+        'strict':True,
+        'parameters':{
+            'type':'object',
+            'properties':{
+                'query':{'type':'string'}
+            },
+            'required':['query'],
+            'additionalProperties':False
+        }
+    }
+}
+
+import json 
+def chat(messages):
+    res = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        tools=[bing_tool_def,]
+    )
+    appendix = []
+    while res.choices[0].message.tool_calls:
+        appendix.append(res.choices[0].message)
+        for tool in res.choices[0].message.tool_calls:
+            if tool.function.name == 'search_and_get_text':
+                args = json.loads(tool.function.arguments)
+                query = args['query']
+                print(args, query)
+                appendix.append({
+                    'role':'tool',
+                    'tool_call_id':tool.id,
+                    'content': search_and_get_text(query)
+                })
+        messages = messages + appendix
+        print(f"sending {len(messages)} messages")
+        res = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+        )
+        appendix = []
+    return res.choices[0].message
